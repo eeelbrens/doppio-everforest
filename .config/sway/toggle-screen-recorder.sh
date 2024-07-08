115e246278f6050465fe55485d44d88fea6c9e67
eeelbrens <130598002+eeelbrens@users.noreply.github.com>
2024-07-09 00:10:47 +0300

    Add files via upload

--- .config/sway/toggle-screen-recorder.sh
+++ .config/sway/toggle-screen-recorder.sh
@@ -4,9 +4,9 @@ pid=$(pgrep wf-recorder)
 status=$?
 
 if [ $status != 0 ]; then
-	wf-recorder -f ~/Videos/$(date +%Hh_%Mm_%Ss_%d_%B_%Y).mkv --audio=alsa_output.pci-0000_03_00.6.analog-stereo.monitor &
-	notify-send "Recording Started"
+  wf-recorder -f ~/Videos/$(date +%Hh_%Mm_%Ss_%d_%B_%Y).mp4 --audio=alsa_output.pci-0000_03_00.6.analog-stereo.monitor &
+  notify-send "Recording Started"
 else
-	pkill wf-recorder &
-	notify-send "Recording Ended and Saved to ~/Videos/"
+  pkill wf-recorder &
+  notify-send "Recording Ended and Saved to ~/Videos/"
 fi
